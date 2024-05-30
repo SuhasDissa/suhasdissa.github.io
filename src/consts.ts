0400a0c7419a0a5794d46d9ad8c0c0c38d14a807
Suhas Dissanayake <suhasdissa@gmail.com>
2024-05-31 00:22:04 +0530

    Add a unique site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Suhas Dissanayake";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "Hi, I'm Suhas Dissanayake, an Android developer from Sri Lanka. Passionate about graphics design, open-source, command lines, and all things Linux!";
